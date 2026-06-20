37b6475f76e1c078971ad5685a13b5d598772063
Rustam Sadreev <rustam17.sb@gmail.com>
2026-06-20 22:44:04 +0300

    feat(cli): add command manager

---
+++ src/cli/cli.application.ts
@@ -0,0 +1,43 @@
+import { CommandName } from './commands/command-name.enum.js';
+import { Command } from './commands/command.interface.js';
+import { CommandParser } from './command.parser.js';
+
+type CommandCollection = Record<string, Command>;
+
+export class CLIApplication {
+  private commands: CommandCollection = {};
+
+  constructor(
+    private readonly defaultCommandName = CommandName.Help,
+  ) { }
+
+  public registerCommands(commandList: Command[]): void {
+    commandList.forEach((command) => {
+      if (Object.hasOwn(this.commands, command.getName())) {
+        throw new Error(`Command ${command.getName()} is already registered.`);
+      }
+
+      this.commands[command.getName()] = command;
+    });
+  }
+
+  public async processCommand(argv: string[]): Promise<void> {
+    const parsedCommand = CommandParser.parse(argv);
+    const [commandName] = Object.keys(parsedCommand);
+    const command = this.getCommand(commandName);
+    const commandArguments = parsedCommand[commandName] ?? [];
+    await command.execute(...commandArguments);
+  }
+
+  private getCommand(commandName: string): Command {
+    return this.commands[commandName] ?? this.getDefaultCommand();
+  }
+
+  private getDefaultCommand(): Command | never {
+    if (!Object.hasOwn(this.commands, this.defaultCommandName)) {
+      throw new Error(`The default command (${this.defaultCommandName}) is not registered.`);
+    }
+
+    return this.commands[this.defaultCommandName];
+  }
+}
--- src/cli/index.ts
+++ src/cli/index.ts
@@ -1,2 +1,4 @@
+export { CLIApplication } from './cli.application.js';
+
 export { HelpCommand } from './commands/help.command.js';
 export { VersionCommand } from './commands/version.command.js';
