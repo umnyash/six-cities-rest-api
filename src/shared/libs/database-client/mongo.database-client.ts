d114a87566d59a922896739bf94682237efdee76
Rustam Sadreev <rustam17.sb@gmail.com>
2026-07-17 22:58:52 +0300

    feat(database-client): retry connection on failure

--- src/shared/libs/database-client/mongo.database-client.ts
+++ src/shared/libs/database-client/mongo.database-client.ts
@@ -1,9 +1,13 @@
 import * as Mongoose from 'mongoose';
 import { inject, injectable } from 'inversify';
+import { setTimeout } from 'node:timers/promises';
 
 import { DatabaseClient } from './database-client.interface.js';
 import { Component } from '../../types/index.js';
 import { Logger } from '../logger/index.js';
+
+const RETRIES_COUNT = 5;
+const RETRY_INTERVAL = 1000;
 
 @injectable()
 export class MongoDatabaseClient implements DatabaseClient {
@@ -22,11 +26,27 @@ export class MongoDatabaseClient implements DatabaseClient {
     }
 
     this.logger.info('Connecting to MongoDB…');
+    let attemptNumber = 0;
 
-    this.mongoose = await Mongoose.connect(uri);
-    this.isConnected = true;
+    while (attemptNumber < RETRIES_COUNT) {
+      attemptNumber++;
 
-    this.logger.info('MongoDB connected.');
+      try {
+        this.mongoose = await Mongoose.connect(uri);
+        this.isConnected = true;
+        this.logger.info('MongoDB connected.');
+        return;
+      } catch (error: unknown) {
+        this.logger.error(error as Error, `MongoDB connection attempt ${String(attemptNumber)} failed.`);
+
+        if (attemptNumber < RETRIES_COUNT) {
+          this.logger.info(`Retrying MongoDB connection in ${String(RETRY_INTERVAL)} ms…`);
+          await setTimeout(RETRY_INTERVAL);
+        }
+      }
+    }
+
+    throw new Error(`MongoDB connection failed after ${String(attemptNumber)} attempts.`);
   }
 
   public async disconnect(): Promise<void> {
