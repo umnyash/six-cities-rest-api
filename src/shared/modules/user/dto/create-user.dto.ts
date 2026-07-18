ea2dab9d7e3c61aa492e2bec66a8f24fb587e758
Rustam Sadreev <rustam17.sb@gmail.com>
2026-07-19 02:32:34 +0300

    feat(user): add DTO for user creation

---
+++ src/shared/modules/user/dto/create-user.dto.ts
@@ -0,0 +1,7 @@
+export class CreateUserDto {
+  public name: string;
+  public email: string;
+  public password: string;
+  public avatarUrl: string;
+  public isPro: boolean;
+}
--- src/shared/modules/user/index.ts
+++ src/shared/modules/user/index.ts
@@ -1,1 +1,2 @@
+export { CreateUserDto } from './dto/create-user.dto.js';
 export { UserEntity, UserModel } from './user.entity.js';
